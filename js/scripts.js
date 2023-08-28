0dc9eb72ee8cad31520b8e7df4a2346437734db2
Evgeny <djek.uait1998@gmail.com>
2023-08-28 16:32:01 +0300

    adap2 commit

--- js/scripts.js
+++ js/scripts.js
@@ -35,25 +35,24 @@ startTimer(timerDuration);
 
 
 
-
 new Swiper ('.slider__slider', {
-    // Optional parameters
+    
     slidesPerView: 1,
     loop: true,
   
-    // If we need pagination
+   
     pagination: {
     el: '.swiper-pagination',
     clickable: true,
     },
   
-    // Navigation arrows
+    
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
   
-    // And if we need scrollbar
+    
     scrollbar: {
     el: '.swiper-scrollbar',
     },
